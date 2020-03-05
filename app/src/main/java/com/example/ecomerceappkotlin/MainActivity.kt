9cb88606fe30e344d13a383bc7c67021ab384f78
ledungcobra <ledungcobra@gmail.com>
2020-03-05 23:26:27 +0700

    Demo local request

--- app/src/main/java/com/example/ecomerceappkotlin/MainActivity.kt
+++ app/src/main/java/com/example/ecomerceappkotlin/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.example.ecomerceappkotlin
 
-import android.content.Context
 import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.shashank.sony.fancytoastlib.FancyToast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,18 +19,31 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btnAdd.setOnClickListener{
+        btnGetData.setOnClickListener {
 
-            sharedPref = getSharedPreferences("addData", Context.MODE_PRIVATE);
-            var myEditor = sharedPref?.edit()
-            myEditor.putString("Xin chao","cac ban");
-            
+            val serverURL = "http://192.168.1.8/PhpTest/newEmptyPHP.php";
+            val requestQ: RequestQueue = Volley.newRequestQueue(this@MainActivity);
+            val stringRequest =  StringRequest(Request.Method.GET,serverURL,Response.Listener <String>{
+                txtHelloWorld.text = it.toString()
+
+            },Response.ErrorListener {
+
+                txtHelloWorld.text = "ERROR: %s".format(it.toString())
+            })
+            requestQ.add(stringRequest)
 
 
         }
 
 
 
+
+
+
+    }
+    private fun showToast(message:String,mode:Int){
+
+        FancyToast.makeText(this,message,FancyToast.LENGTH_SHORT,mode,false).show();
 
     }
 }
---
+++ app/src/main/java/com/example/ecomerceappkotlin/ShowJsonObjectActivity.kt
@@ -0,0 +1,12 @@
+package com.example.ecomerceappkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ShowJsonObjectActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_show_json_object)
+    }
+}
